8865c91a3f89527812d0ad7893cd9ed2b28ffdad
dykim <dykim@fastcat.co>
2015-08-09 18:55:05 +0900

    메인 UI작성

--- app/src/main/java/com/exitpoint/kkakkung/Sample2.java
+++ app/src/main/java/com/exitpoint/kkakkung/Sample2.java
@@ -2,16 +2,42 @@ package com.exitpoint.kkakkung;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 public class Sample2 extends ActionBarActivity {
 
+    int deviceHeight;
+    int deviceWidth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample2);
+
+        //디바이스 사이즈를 구합시다.
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        deviceWidth = metrics.widthPixels;
+        deviceHeight = metrics.heightPixels;
+
+        //레이아웃 사이즈 동적 변경을 위한 레이아웃 불러옴
+        //만들고
+        LinearLayout layout_friendslistpage1,layout_friendslistpage1_1,layout_friendslistpage1_2,layout_friendslistpage1_3;
+        //매핑해주고
+        layout_friendslistpage1=(LinearLayout) findViewById(R.id.layout_friendslistpage1);
+        layout_friendslistpage1_1=(LinearLayout) findViewById(R.id.layout_friendslistpage1_1);
+        layout_friendslistpage1_2=(LinearLayout) findViewById(R.id.layout_friendslistpage1_2);
+        layout_friendslistpage1_3=(LinearLayout) findViewById(R.id.layout_friendslistpage1_3);
+
+        layout_friendslistpage1_1.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,deviceHeight/6+30));
+        layout_friendslistpage1_2.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,deviceHeight/6+30));
+        layout_friendslistpage1_3.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,deviceHeight/6+30));
+        System.out.println("deviceWidth---------"+deviceWidth);
+
     }
 
     @Override
